9d952732223bf65f4b5743c45f8fcb6646c4df2f
newneo4 <105571999+newneo4@users.noreply.github.com>
2022-05-25 23:26:45 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,17 +17,18 @@ int main(){
         cout<<"Desea recorrer el letrero(s/n): ";
         cin>>respuesta;
         if(respuesta == "s"){
-            while(contador1 >= 0){
+            while(contador1){
                 for(int i = contador1, j = contador2; i >= 0; --i, --j){
                     cadena[i] = copiar[j];
+                    if(j < 0){
+                        j = 11;
+                    }        
                 }
-                contador1 = contador1 - 1;
-                contador2 = contador2 - 1;
-            }
-            for(int i = 0; i < 12; i++){
+                for(int i = 0; i < 12; i++){
                 cout<<cadena[i];
-            }
-            cout<<"\n";
+                }
+                cout<<"\n"; 
+                contador1 = false;
         }
         else if(respuesta == "n"){
             entrada = false;
@@ -35,6 +36,8 @@ int main(){
         else{
             cout<<"Vuelva a intentar"<<endl;
         }
+        contador1 = 11;
+        contador2 -= 1;
     }
 
 
